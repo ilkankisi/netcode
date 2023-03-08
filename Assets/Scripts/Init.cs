c668b1583f8a50d8eedcbbc18d820cdc417dc4fd
ilkankisi <65829605+ilkankisi@users.noreply.github.com>
2023-03-09 00:55:13 +0300

    Authentication was done

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        if (UnityServices.State==ServicesInitializationState.Initialized)
+        {
+
+            AuthenticationService.Instance.SignedIn += OnSignedIn;
+
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+            if (AuthenticationService.Instance.IsSignedIn)
+            {
+                string username = PlayerPrefs.GetString(key: "username");
+                if (username=="")
+                {
+                    username = "Player";
+                    PlayerPrefs.SetString(key: "username", value: username);
+                }
+
+                SceneManager.LoadSceneAsync("MainMenu");
+            }
+        }
+    }
+
+    private void OnSignedIn()
+    {
+        Debug.Log(message:$"Player Id: {AuthenticationService.Instance.PlayerId}");
+        Debug.Log(message:$"Token: {AuthenticationService.Instance.AccessToken}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
